1e970d6701a820daed19e91023f456f81d0bca81
pritesh <pritesh3278@gmail.com>
2023-05-15 16:29:48 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>hiidsfsfs</Button>
+    </div>
+  );
+}
+
+export default App;
